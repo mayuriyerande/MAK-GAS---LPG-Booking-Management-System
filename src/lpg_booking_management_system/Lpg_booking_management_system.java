14f99fa5d4f38e366f5885698aa19c69519c2775
mayuriyerande <mayuriyerande@gmail.com>
2023-03-22 13:24:22 +0530

    mayuri commit

---
+++ src/lpg_booking_management_system/Lpg_booking_management_system.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lpg_booking_management_system;
+
+/**
+ *
+ * @author 91738
+ */
+public class Lpg_booking_management_system {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
